dd3d172b599df850659771b79712a5e3ab9609a6
Clayton Matulich <claytonmatulich13@gmail.com>
2024-09-08 15:49:17 -0700

    init tanstack router

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,4 @@
 import { createHTTPServer } from "@trpc/server/adapters/standalone"
-// import cors from 'cors'
 import { router } from './trpc'
 import { movieRouter } from './routers/movie.ts'
 
@@ -10,9 +9,7 @@ const appRouter = router({
 export type AppRouter = typeof appRouter
 
 const server = createHTTPServer({
-  // middleware: cors(),
   router: appRouter,
-  
 })
 
 server.listen(3000);
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { routeTree } from "./routeTree.gen";
+import { trpc } from "@/utils/trpc";
+import { httpBatchLink } from "@trpc/client";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:3000",
+    }),
+  ],
+});
+
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </trpc.Provider>
+    </StrictMode>,
+  );
+}
